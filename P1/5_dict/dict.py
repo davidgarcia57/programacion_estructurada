7874eff4a4a5469c4cfa1728f70e3d371dc7578f
davidgarcia57 <david_3141240195@utd.edu.mx>
2025-06-06 09:35:04 -0600

    Update dict.py

--- P1/5_dict/dict.py
+++ P1/5_dict/dict.py
@@ -10,3 +10,50 @@
 import os
 os.system("clear")
 
+paises=["México","Brasil","Canada","España"]
+
+pais1={"nombre":"México"
+       "capital":"CDMX",
+       "poblacion":12000000,
+       "idioma":"español",
+       "status":True
+       }
+
+pais2={"nombre":"Brasil"
+       "capital":"Brasilia",
+       "poblacion":14000000,
+       "idioma":"portugues",
+       "status":True
+       }
+
+pais3={"nombre":"Canada"
+       "capital":"Otawa",
+       "poblacion":10000000,
+       "idioma":["ingles","frances"],
+       "status":True
+       }
+
+print(pais1)
+
+for i in pais 1:
+   print(f"{i}= {pais1[i]}")
+
+#Agregar un atributo a un objeto
+pais1.["altitud"]=3000
+for i in pais 1:
+   print(f"{i}= {pais1[i]}")
+
+#Modificar un valor o item que ya existe
+pais1.update("altitud":2500)
+for i in pais 1:
+   print(f"{i}= {pais1[i]}")
+
+#Eliminar el ultimo atributo de un objeto
+pais1.popitem()
+for i in pais 1:
+   print(f"{i}= {pais1[i]}")
+
+"Eliminar un atributo a eleccion
+pais1.pop("status")
+for i in pais 1:
+   print(f"{i}= {pais1[i]}")
