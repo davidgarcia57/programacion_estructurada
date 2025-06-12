b59c6825cebb3d239f382baa4224cd00350bee83
davidgarcia57 <david_3141240195@utd.edu.mx>
2025-06-12 09:26:55 -0600

    clase del jueves

--- P2/1_proyecto_peliculas/peliculas.py
+++ P2/1_proyecto_peliculas/peliculas.py
@@ -21,3 +21,43 @@ def mostrarPeliculas():
             print(f"{i+1} : {peliculas[i]}")
     else:
         print("No hay peliculas en este momento en el sistema::.")
+
+def LimpiarPelicula():
+    borrarPantalla()
+    print(".::Limpiar todas las Peliculas::.")
+    resp=input("¿Deseas borrar todas las peliculas del sistema? (Si/No)").lower().strip()
+    if resp=="si":
+        peliculas.clear()
+        print("\n\t\t:::La operación se realizó con exito")
+
+def buscarPelicula():
+    borrarPantalla()
+    print(".::Buscar Peliculas::.")
+    pelicula_buscar=input("Ingrese el nombre de la pelicula a buscar: ").upper().strip()
+    if not(pelicula_buscar in peliculas):
+        print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
+    else:
+        encontro=0
+        for i in range(0, len(peliculas)):
+            if pelicula_buscar==peliculas[i]:
+                print(f"\n\tLa pelicula{pelicula_buscar}se encontro en el casillero: {i+1}")
+                encontro+=1
+            print(f"\nTenemos{encontro} pelicula(s) con este titulo")
+            print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
+
+def modificarPeliculas():
+    borrarPantalla()
+    print(".::Modificar Peliculas::.")
+    pelicula_buscar=input("Ingrese el nombre de la pelicula a buscar: ").upper().strip()
+    if not(pelicula_buscar in peliculas):
+        print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
+    else:
+        encontro=0
+        for i in range(0, len(peliculas)):
+            if pelicula_buscar==peliculas[i]:
+                resp=input("Deseas actualizar la pelicula (Si/No)").lower()
+                if resp=="si":
+                    peliculas[i]=input("\n Introduce el nuevo nombre de la pelicula").upper().strip()
+                    encontro+=1
+                    print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
+            print(f"\nSe modifico{encontro} pelicula(s) con este titulo")
