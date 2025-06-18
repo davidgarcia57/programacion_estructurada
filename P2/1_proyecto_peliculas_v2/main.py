d351ca268429a309707a6502edfae4ca9cde4516
davidgarcia57 <david_3141240195@utd.edu.mx>
2025-06-18 09:46:22 -0600

    clase del miercoles

--- P2/1_proyecto_peliculas_v2/main.py
+++ P2/1_proyecto_peliculas_v2/main.py
@@ -4,7 +4,7 @@ Modificar, Mostrar, Buscar, Limpiar una lista de peliculas.
 
 Notas:
 1.- Utilizar funciones y mandar llamar desde el archivo (modulo)
-2.- Utilizar una lista para almacenar los nombres de las peliculas
+2.- Utilizar dict para almacenar los atributos (nombre,categoria,clasificacion,genero,idioma) de peliculas
 '''
 
 import os
@@ -16,28 +16,28 @@ opcion=True
 
 while opcion:
     peliculas.borrarPantalla()
-    print("\n\t 1.- Agregar pelicula \n\t 2.- Borrar pelicula \n\t 3.- Modificar pelicula \n\t 4.- Mostrar peliculas " \
-    "\n\t 5.- Buscar pelicula \n\t 6.- Limpiar lista de peliculas \n\t 7.- Salir")
+    print("\n\t 1.- Crear \n\t 2.- Borrar \n\t 3.- Mostrar \n\t 4.- Agregar Caracteristica " \
+    "\n\t 5.- Modificar Caracteristica \n\t 6.- Borrar \n\t 7.- Salir")
     opcion=input("\n\t Selecciona una opcion: ").upper()
     
     match opcion:
         case "1":
-            peliculas.agregarPelicula()
+            peliculas.crearPeliculas()
             peliculas.esperarTecla()
         case "2":
             peliculas.borrarPelicula()
             peliculas.esperarTecla()
         case "3":
-            peliculas.modificarPeliculas()
-            peliculas.esperarTecla()
-        case "4":
             peliculas.mostrarPeliculas()
             peliculas.esperarTecla()
+        case "4":
+            peliculas.agregarCaracteristicaPeliculas()
+            peliculas.esperarTecla()
         case "5":
-            peliculas.buscarPelicula()
+            peliculas.modificarCaracteristicaPeliculas()
             peliculas.esperarTecla()
         case "6":
-            peliculas.LimpiarPelicula()
+            peliculas.borrarCaracteristicaPeliculas()
             peliculas.esperarTecla()
         case "7":
             print("\n\t Terminaste la ejecucion del programa gracias por usarlo")
--- P2/1_proyecto_peliculas_v2/peliculas.py
+++ P2/1_proyecto_peliculas_v2/peliculas.py
@@ -1,4 +1,13 @@
-peliculas=[]
+#Dict u objeto que permita almacenar los siguientes atributos: (nombre, categoria, clasificacion, genero, idioma)
+
+#pelicula = {
+#    "nombre": "",
+#    "categoria": "",
+#    "clasificacion": "",
+#    "genero": "",
+#    "idioma": ""
+
+pelicula={}
 
 def borrarPantalla():
     import os
@@ -7,22 +16,10 @@ def borrarPantalla():
 def esperarTecla():
     input("\n\t Presiona una tecla para continuar...")
 
-def agregarPelicula():
-
+def crearPeliculas():
     borrarPantalla()
-    print(".::Modificar Peliculas::.")
-    pelicula_buscar=input("Ingrese el nombre de la pelicula a buscar: ").upper().strip()
-    if not(pelicula_buscar in peliculas):
-        print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
-    else:
-        encontro=0
-        for i in range(0, len(peliculas)):
-            if pelicula_buscar==peliculas[i]:
-                resp=input("Deseas actualizar la pelicula (Si/No)").lower()
-                if resp=="si":
-                    peliculas[i]=input("\n Introduce el nuevo nombre de la pelicula").upper().strip()
-                    encontro+=1
-                    print("\n\t.::Esta pelicula a buscar no existe en el sistema::.")
-            print(f"\nSe modifico{encontro} pelicula(s) con este titulo")
-
+    print("\n\t\t\t\tCrear Pelicula")
+    pelicula.update(input("\n\t Nombre de la pelicula: ").strip().upper())
+    print("\n\t Pelicula creada exitosamente!")
+    esperarTecla()
 
